ec80705433ccc2971b971c21de5c620d8a48051e
Qsyoma <kolesnikov.yk@gmail.com>
2025-03-21 21:07:30 +0300

    first DZ 1

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -9,5 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val settingsButton = findViewById<Button>(R.id.main_button_settings)
+
+     settingsButton.setOnClickListener {
+         val settingsIntent = Intent(this, SettingsActivity::class.java)
+         startActivity(settingsIntent)
+     }
     }
 }
---
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,22 @@
+package com.practicum.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val backButton = findViewById<ImageButton>(R.id.settings_back_button)
+
+        backButton.setOnClickListener {
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
+            finish()
+        }
+    }
+}
