27dcd76b221fb75fb864e69514b282948719e290
Qsyoma <kolesnikov.yk@gmail.com>
2025-03-22 15:43:17 +0300

    first DZ 2

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -12,11 +12,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val searchButton = findViewById<Button>(R.id.main_button_search)
         val settingsButton = findViewById<Button>(R.id.main_button_settings)
+        val mediaButton = findViewById<Button>(R.id.main_button_media)
 
-     settingsButton.setOnClickListener {
-         val settingsIntent = Intent(this, SettingsActivity::class.java)
-         startActivity(settingsIntent)
-     }
+        searchButton.setOnClickListener {
+            val searchButtonIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchButtonIntent)
+        }
+
+        settingsButton.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
+
+        mediaButton.setOnClickListener {
+            val mediaButtonIntent = Intent(this, MediaActivity::class.java)
+            startActivity(mediaButtonIntent)
+        }
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -15,6 +15,8 @@ class SearchActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
     }
 }
