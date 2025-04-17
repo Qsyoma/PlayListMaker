823ac322351238d98f7e0cf76bdcdd9de49c3a05
Qsyoma <kolesnikov.yk@gmail.com>
2025-04-17 17:04:26 +0300

    pre dark theme

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,22 +1,49 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val backButton = findViewById<ImageButton>(R.id.search_back_button)
+        val searchEditText = findViewById<EditText>(R.id.search_edit_text)
+        val clearButton = findViewById<ImageButton>(R.id.clear_text_button)
 
 
+        backButton.setOnClickListener {
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
+            finish()
+        }
+
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+
+        })
+        clearButton.setOnClickListener{
+            searchEditText.text.clear()
         }
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,10 +1,12 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity: AppCompatActivity() {
@@ -15,6 +17,25 @@ class SettingsActivity: AppCompatActivity() {
         val backButton = findViewById<ImageButton>(R.id.settings_back_button)
         val barSearchButton = findViewById<LinearLayout>(R.id.bar_search_button)
         val barMediaButton = findViewById<LinearLayout>(R.id.bar_media_button)
+        val shareButton = findViewById<RelativeLayout>(R.id.settings_button_share)
+        val supportButton = findViewById<RelativeLayout>(R.id.settings_support_button)
+
+        supportButton.setOnClickListener {
+            val message = "Привет Android это круто"
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("kolesnikov.yk@ya.ru"))
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, message)
+            startActivity(shareIntent)
+        }
+
+        shareButton.setOnClickListener {
+            val message = "https://practicum.yandex.ru/android-developer"
+            val shareIntent = Intent(Intent.ACTION_SEND).apply { type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, message)
+            }
+            startActivity(shareIntent)
+        }
 
         barMediaButton.setOnClickListener {
             val barMediaIntent = Intent(this, MediaActivity::class.java)
